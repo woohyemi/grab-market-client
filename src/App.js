2ef7914ad06a94445b17a7d5ae23d333d45ee794
woohyemi <xxtakehyem@gmail.com>
2021-09-14 16:44:22 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
 import MainPage from "./main";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPage />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
